9c90e8f662b093b7444a0a7b8b04f8b89fc0d38c
Rahul <rahul731999@gmail.com>
2019-10-12 16:55:09 +0530

    bacon: build Health hal packages

--- bacon.mk
+++ bacon.mk
@@ -165,6 +165,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+# Health
+PRODUCT_PACKAGES += \
+	android.hardware.health@2.0-impl \
+	android.hardware.health@2.0-service
+
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8974
